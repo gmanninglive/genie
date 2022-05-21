5c924de67bc9e1663a9f1076541fa89169c55075
gmanninglive <gmanning@live.co.uk>
2022-05-21 17:25:57 +0100

    Add array of make commands to each 'command'

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module main
 
 go 1.18
 
-require github.com/aymerick/raymond v2.0.2+incompatible
+require (
+	github.com/aymerick/raymond v2.0.2+incompatible
+	github.com/manifoldco/promptui v0.9.0
+)
 
-require github.com/cheekybits/genny v1.0.0 // indirect
+require (
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+)
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 
 	"github.com/aymerick/raymond"
+	"github.com/manifoldco/promptui"
 )
 
 func check(e error) {
@@ -29,61 +30,90 @@ func readflags() Flags {
   return res
 }
 
-type Command struct {
-  Title string
+type MakeCMD struct {
   Directory string
   Filename string
   Template string
-  CtxVars map[string]string
   Output string
+}
+
+type Command struct {
+  Title string
+  Make []MakeCMD
+  CtxVars map[string]string
+  Base string
 }
 
 type Config []Command
 
 func loadConfig(config_location string) Config {
   var config Config
-  config_dir := filepath.Dir(config_location)
   
   f, err := os.ReadFile(config_location)
   check(err)
-
+  
   json.Unmarshal([]byte(f), &config)
-
+  
   for i := 0; i < len(config); i++ {
-    config[i].Template = filepath.Join(config_dir, config[i].Template)
-    config[i].Output = filepath.Join(config[i].Directory, config[i].Filename)
+    config[i].Base = filepath.Dir(config_location)
   }
   
   return config
 }
 
-func runCommand(command Command) string {
-  parse, err := raymond.ParseFile(command.Template)
-  check(err)
-  withCtx, err := parse.Exec(command.CtxVars)
-  check(err)
+func runCommand(command Command) {
+  for i := 0; i < len(command.Make); i++ {
+    template_path := filepath.Join(command.Base, command.Make[i].Template)
 
-  return withCtx
+    parse, err := raymond.ParseFile(template_path)
+    check(err)
+    withCtx, err := parse.Exec(command.CtxVars)
+    check(err)
+
+    command.Make[i].Output = filepath.Join(command.Make[i].Directory, command.Make[i].Filename)
+
+    writeFile(command.Make[i], []byte(withCtx))
+    fmt.Printf("created: %s\n", command.Make[i].Output)
+  }
 }
 
-func writeFile(command Command, template []byte) {
-  if _, err := os.Stat(command.Directory); os.IsNotExist(err) {
-    err := os.MkdirAll(command.Directory, 0700)
+func writeFile(make MakeCMD, template []byte) {
+  if _, err := os.Stat(make.Directory); os.IsNotExist(err) {
+    err := os.MkdirAll(make.Directory, 0700)
     check(err)
   }
 
-  err := os.WriteFile(command.Output, []byte(template), 0644)
+  err := os.WriteFile(make.Output, []byte(template), 0644)
   check(err)
 }
 
 func main() {
   flags := readflags()
   config := loadConfig(flags.Config)
+  options := make([]string, len(config))
+
+  for i := 0; i < len(config); i++ {
+    options[i] = config[i].Title
+  }
+
+  prompt := promptui.Select{
+		Label: "Select a Command",
+		Items: options,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
   
   for i := 0; i < len(config); i++ {
-    output := runCommand(config[i])
-    writeFile(config[i], []byte(output))
-    fmt.Printf("%s completed, created: %s\n", config[i].Title, config[i].Output)
+    if config[i].Title == result {
+      runCommand(config[i])
+
+      return
+    }
   }
 }
  
